6f0eea591629b34f183708251f9c862df522509e
CaioSousa32 <caio123c@gmail.com>
2025-03-26 16:46:07 -0300

    Adicionando tipo do curso

--- app/Screens/Cursos.tsx
+++ app/Screens/Cursos.tsx
@@ -29,35 +29,35 @@ const Cursos = () => {
               activeOpacity={0.5} // Reduz a opacidade ao pressionar
               onPress={() => router.push("/Screens/Cursos/Adm1")}>
                 <Text style={styles.buttonText}>Administração</Text>
-                <Text style={styles.buttonText}>(Curso Superior)</Text>
+                <Text style={styles.buttonText}>(Superior Bacharelado)</Text>
                 </TouchableOpacity>
 
             <TouchableOpacity style={styles.button} 
               activeOpacity={0.5} // Reduz a opacidade ao pressionar
               onPress={() => router.push("/Screens/Cursos/Log1")}>
                 <Text style={styles.buttonText}>Logística</Text>
-                <Text style={styles.buttonText}>(Curso Técnico)</Text>
+                <Text style={styles.buttonText}>(Técnico Subsequente)</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.button} 
               activeOpacity={0.5} // Reduz a opacidade ao pressionar
               onPress={() => router.push("/Screens/Cursos/Gq1")}>
                 <Text style={styles.buttonText}>Gestão da Qualidade</Text>
-                <Text style={styles.buttonText}>(Curso Superior)</Text>
+                <Text style={styles.buttonText}>(Superior Tecnológo)</Text>
                 </TouchableOpacity>
 
             <TouchableOpacity style={styles.button} 
               activeOpacity={0.5} // Reduz a opacidade ao pressionar
               onPress={() => router.push("/Screens/Cursos/Ipi1")}>
                 <Text style={styles.buttonText}>Informática Para Internet</Text>
-                <Text style={styles.buttonText}>(Curso Técnico)</Text>
+                <Text style={styles.buttonText}>(Técnico Subsequente)</Text>
             </TouchableOpacity>
 
             <TouchableOpacity style={styles.button} 
               activeOpacity={0.5} // Reduz a opacidade ao pressionar
               onPress={() => router.push("/Screens/Cursos/Tsi1")}>
                 <Text style={styles.buttonText}>Tecnologia em Sistemas para Internet</Text>
-                <Text style={styles.buttonText}>(Curso Superior)</Text>
+                <Text style={styles.buttonText}>(Superior Tecnológo)</Text>
             </TouchableOpacity>
 
             </View>
