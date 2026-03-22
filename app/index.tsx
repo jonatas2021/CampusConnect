0d1c62643ae86c0299fecfa3e135d77dc29f7ab6
CaioSousa32 <caio123c@gmail.com>
2026-03-21 21:14:16 -0300

    Teste

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LoadingScreen from './Screens/LoadingScreen'; // o arquivo renomeado do seu antigo index.jsx
+import LoadingScreen from './Screens/LoadingScreen'; // o arquivo renomeado do seu antigo index.jsx 
 
 export default function App() {
   return <LoadingScreen />;
